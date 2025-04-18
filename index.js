7ca8c623e3d635eab1f393acf60f9a22959a1200
guy-car <guil.crn@gmail.com>
2025-04-18 17:46:10 -0400

    uibuild

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const Note = require('./models/note')
 
 const app = express()
 
-let notes = []
-
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -14,16 +12,26 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(requestLogger)
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
 app.use(express.static('dist'))
 app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 ////////////////
-////Getting initial notes with Mongoose
+////GET initial notes with Mongoose
 ////////////////
 
 app.get('/api/notes', (request, response) => {
@@ -33,17 +41,23 @@ app.get('/api/notes', (request, response) => {
 })
 
 ////////////////
-////Getting a specific note with Mongoose
+////GET a specific note with Mongoose
 ////////////////
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
   })
+  .catch(error =>  next(error))
 })
 
 ////////////////
-////Creating a new note with Mongoose
+////POST a new note with Mongoose
 ////////////////
 
 app.post('/api/notes', (request, response) => {
@@ -66,21 +80,47 @@ app.post('/api/notes', (request, response) => {
 })
 
 ////////////////
-////
+////PUT update a single note
 ////////////////
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter((note) => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const {content, important} = request.body
 
-  response.status(204).end()
+  Note.findById(request.params.id)
+  .then(note => {
+    if (!note) {
+      return response.status(404).end()
+    }
+
+    note.content = content
+    note.important = important
+
+    return note.save().then((updatedNote) => {
+      response.json(updatedNote)
+    })
+  })
+  .catch(error => next(error))
 })
+
+////////////////
+////DELETE a note
+////////////////
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+////////
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
-
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
