b86f7a5f6fc72467aa2170823987efb6ac9b7db9
guy-car <guil.crn@gmail.com>
2025-04-24 13:25:47 -0400

    fixed eslint bugs, now stable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import globals from "globals";
+import globals from 'globals'
 import js from '@eslint/js'
 import stylisticJs from '@stylistic/eslint-plugin-js'
-import { defineConfig } from "eslint/config";
 
 
-export default defineConfig([
+export default [
   js.configs.recommended,
-  { files: ["**/*.js"], 
-    languageOptions: { 
-      sourceType: "commonjs",
-    globals: { ...globals.node },
-  ecmaVersion: 'latest',
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
     },
     plugins: { 
       '@stylistic/js': stylisticJs,
@@ -26,9 +26,9 @@ export default defineConfig([
       'arrow-spacing': ['error', { before: true, after: true }],
       'no-console': 'off',
     }, 
+    
   },
-  {
-    ignores: ['dist/**']
+  { 
+    ignores: ['dist/**'], 
   },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+]
